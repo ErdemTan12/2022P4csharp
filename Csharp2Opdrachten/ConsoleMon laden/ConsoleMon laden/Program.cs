517dc3754603aea60e706785504946f17c3efeb8
GreenVan72 <67523558+GreenVan72@users.noreply.github.com>
2022-05-09 10:11:06 +0200

    opdrachten

---
+++ Csharp2Opdrachten/ConsoleMon laden/ConsoleMon laden/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMon_laden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
